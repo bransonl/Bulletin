0a03fa2f274e58e60c02920a1780c310692f2737
Branson Lee <brannsonl@gmail.com>
2017-07-16 16:59:15 +0700

    Add wrapper function to unwrap data and raise error if invalid

---
+++ server/bulletin/common/validation.py
@@ -0,0 +1,18 @@
+from functools import wraps
+
+from flask import request
+
+from bulletin.common import errors
+
+
+def unwrap_data(schema):
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            data, validation_errors = schema().load(request.get_json() or {})
+            if validation_errors:
+                raise errors.InvalidData(errors=validation_errors)
+            kwargs['data'] = data
+            return f(*args, **kwargs)
+        return wrapped
+    return wrapper
--- server/bulletin/controllers/auth.py
+++ server/bulletin/controllers/auth.py
@@ -2,10 +2,9 @@ import base64
 import hashlib
 
 import bcrypt
-from flask import request
 
 from bulletin import app, db
-from bulletin.common import errors
+from bulletin.common import errors, validation
 from bulletin.libs import jwttoken
 from bulletin.schemas.auth import AccessTokenSchema, AuthErrorMessage, \
     LoginSchema, PasswordSchema, SignupSchema, UsernameSchema
@@ -17,28 +16,22 @@ def _encode_password(password):
 
 
 @app.route('/username', methods=['POST'])
-def validate_username():
-    data, validation_errors = UsernameSchema().load(request.get_json() or {})
-    if validation_errors:
-        raise errors.InvalidData(errors=validation_errors)
+@validation.unwrap_data(UsernameSchema)
+def validate_username(data):
     return UsernameSchema(wrap=True).to_json({
         'username': data.get('username')
     })
 
 
 @app.route('/password', methods=['POST'])
-def validate_password():
-    data, validation_errors = PasswordSchema().load(request.get_json() or {})
-    if validation_errors:
-        raise errors.InvalidData(errors=validation_errors)
+@validation.unwrap_data(PasswordSchema)
+def validate_password(data):
     return PasswordSchema(wrap=True).to_json({'password': ''})
 
 
 @app.route('/login', methods=['POST'])
-def login():
-    data, validation_errors = LoginSchema().load(request.get_json() or {})
-    if validation_errors:
-        raise errors.InvalidData(errors=validation_errors)
+@validation.unwrap_data(LoginSchema)
+def login(data):
     username, password = data.get('username'), data.get('password')
     user = UserModel.query.filter_by(username=username).first()
 
@@ -55,10 +48,8 @@ def login():
 
 
 @app.route('/signup', methods=['POST'])
-def signup():
-    data, validation_errors = SignupSchema().load(request.get_json() or {})
-    if validation_errors:
-        raise errors.InvalidData(errors=validation_errors)
+@validation.unwrap_data(SignupSchema)
+def signup(data):
     username, password = data.get('username'), data.get('password')
     hashed = bcrypt.hashpw(_encode_password(password), bcrypt.gensalt())
     user = UserModel(username=username, password=hashed.decode('utf-8'))
