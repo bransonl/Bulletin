2eeb5dd54626aaac6b8782a1dd373a64de8c9c17
Branson Lee <brannsonl@gmail.com>
2018-01-11 00:25:34 -0500

    Add dropdown menus in header

--- client/src/components/home/header.component.tsx
+++ client/src/components/home/header.component.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import {MdDashboard, MdSettings} from "react-icons/lib/md";
+
 import {UserAction} from "../../state/actions/user.action";
 
 interface HeaderProps {
@@ -9,33 +11,46 @@ interface HeaderProps {
 const HeaderComponent: React.SFC<HeaderProps> = (props) => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <a className="navbar-brand" href="#">Bulletin</a>
-
-    <button
-      type="button"
-      className="navbar-toggler"
-      data-toggle="collapse"
-      data-target="#navbarContent"
-      aria-controls="navbarContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"/>
-    </button>
-
-    <div className="collapse navbar-collapse" id="navbarContent">
-      {
-        // TODO: uncomment this when ready
-        /*<form className="form-inline my-2 my-lg-0 search-bar">
-        <input type="text" className="form-control mr-sm-2" placeholder="Search" aria-label="Search"/>
-        <button type="submit" className="btn btn-outline-success my-2 my-sm-0">Search</button>
-        </form>*/
-      }
-      <ul className="navbar-nav justify-content-end ml-auto">
-        <button type="button" className="btn btn-light" onClick={props.logout}>
-          Log Out
+    <ul className="navbar-nav justify-content-end ml-auto">
+      <div className="dropdown">
+        <button
+          type="button"
+          className="btn btn-light"
+          id="board-dropdown-button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <MdDashboard size={20} />
         </button>
-      </ul>
-    </div>
+        <div
+          className="dropdown-menu dropdown-menu-right"
+          aria-labelledby="board-dropdown-button"
+        >
+          <a className="dropdown-item" href="javascript:void(0)">Create board</a>
+          <a className="dropdown-item" href="javascript:void(0)">Manage boards</a>
+        </div>
+      </div>
+      <div className="dropdown show">
+        <button
+          type="button"
+          className="btn btn-light"
+          id="settings-dropdown-button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <MdSettings size={20} />
+        </button>
+        <div
+          className="dropdown-menu dropdown-menu-right"
+          aria-labelledby="settings-dropdown-button"
+        >
+          <a className="dropdown-item" href="javascript:void(0)">Account</a>
+          <a className="dropdown-item" href="javascript:void(0)" onClick={props.logout}>Log Out</a>
+        </div>
+      </div>
+    </ul>
   </nav>
 );
 
