2feee6f3a94641e24982fefdd90c22c097086fce
Branson Lee <brannsonl@gmail.com>
2018-01-11 21:01:22 -0500

    Remove unnecessary prop field in ProtectedRoute

--- client/src/components/router/protectedRoute.component.tsx
+++ client/src/components/router/protectedRoute.component.tsx
@@ -7,27 +7,26 @@ import {AuthRequirement} from "../../router";
 interface ProtectedRouteProps extends RouteProps {
   authRequirement: AuthRequirement,
   isAuthenticated: boolean,
-  component: React.ComponentType,
-  componentProps?: any
+  component: React.ComponentType<any>,
 }
 
 const ProtectedRoute: React.SFC<ProtectedRouteProps> = (props: ProtectedRouteProps) => {
-  const {authRequirement, isAuthenticated, component: C, componentProps, ...rest} = props;
+  const {authRequirement, isAuthenticated, component: C, ...rest} = props;
   return (
     <Route
       {...rest}
       render={(props) => {
         switch(authRequirement) {
           case AuthRequirement.None:
-            return <C {...props} {...componentProps} />;
+            return <C {...rest} />;
           case AuthRequirement.Authenticated:
             if (isAuthenticated) {
-              return <C {...props} {...componentProps} />;
+              return <C {...rest} />;
             }
             return <Redirect to="/login" />;
           case AuthRequirement.Unauthenticated:
             if (!isAuthenticated) {
-              return <C {...props} {...componentProps} />;
+              return <C {...rest} />;
             }
             return <Redirect to="/" />;
         }
